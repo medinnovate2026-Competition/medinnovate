9dab57bda11089a286346fbc393ba7d506c7a9ee
crazyelf-ai <sushmitmorey1805@gmail.com>
2026-05-04 01:28:17 +0530

    fix: port issue

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 /* ================= DB ================= */
 const db = mysql.createPool({
   host: process.env.DB_HOST || "127.0.0.1",
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
